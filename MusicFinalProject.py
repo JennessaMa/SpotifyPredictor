f651816fc1ac9e42e6cc024c5a72a6f81f3713da
Jennessa Ma <jennessa19ma@gmail.com>
2020-12-06 12:53:16 -0800

    added similarity calculations

--- MusicFinalProject.py
+++ MusicFinalProject.py
@@ -2,8 +2,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 import configparser
+import math
 
-const NUM_FEATURES = 11
+NUM_FEATURES = 11
+NUM_SONGS = 79
+THRESHOLD = 0.9966892120292986 #taken from a song I decided was really good in the "missed hits"
 
 #AUTHENTICATION PROCESS
 cid = "6cf836da1edc44bf9d696846615b1083"
@@ -20,49 +23,47 @@ sp = spotipy.Spotify(auth_manager=token)
 #HELPER FUNCTIONS
 #passes in 2 arrays containing audio features of 2 songs
 #returns a similarity value
-def getDotProd(vector1, vector2):
+def getDotProd(song1, song2):
     product = 1;
     for i in range(NUM_FEATURES):
-        product += (vector1[i] * vector2[i])
+        temp = song1[i] * song2[i]
+        product += (song1[i] * song2[i])
     return product
+
+#get the magnitude of a vector
+def getMagnitude(song1):
+    sum = 0
+    for i in range(NUM_FEATURES):
+        sum += (song1[i] * song1[i])
+    return math.sqrt(sum)
+
+#equation for cosine similarity used in content-based filtering
+# = ratio between dot product and magnitudes multiplied
+# will return a value between 0 and 1, with 1 being the most similar to each other
+def getSimilarity(song1, song2):
+    numerator = getDotProd(song1, song2)
+    denominator = getMagnitude(song1) * getMagnitude(song2)
+    return numerator / denominator
+
+def getAvgSimilarity(song1_uri, song1_vector, cmpr_dict):
+    sum = 0
+    for uri_key in cmpr_dict:
+        if (song1_uri == uri_key):
+            pass
+        else:
+            sum += getSimilarity(song1_vector, cmpr_dict[uri_key])
+    return sum / NUM_SONGS
 
 #passes in a list of uris
 #returns a dictionary that maps a uri key to an array of features {song_uri : [features]}
 def getAudioFeatures(uri_list):
-    artists = []
-    tracks = []
-    danceability = []
-    energy = []
-    key = []
-    loudness = []
-    mode = []
-    speechiness = []
-    acousticness = []
-    instrumentalness = []
-    liveness = []
-    valence = []
-    tempo = []
-
+    dict = {}
     for uri in uri_list:
-        features = sp.audio_features(uri)
-        track = sp.track(uri)
-        tracks.append(track["name"])
-        artists.append(track["album"]["artists"][0]["name"])
-        for f in features:
-            danceability.append(f['danceability'])
-            energy.append(f['energy'])
-            key.append(f['key'])
-            loudness.append(f['loudness'])
-            mode.append(f['mode'])
-            speechiness.append(f['speechiness'])
-            acousticness.append(f['acousticness'])
-            instrumentalness.append(f['instrumentalness'])
-            liveness.append(f['liveness'])
-            valence.append(f['valence'])
-            tempo.append(f['tempo'])
-
-    return {"danceability": danceability, "energy": energy, "key": key, "loudness": loudness, "mode": mode, "speechiness": speechiness,
-            "acousticness": acousticness, "instrumentalness": instrumentalness, "liveness": liveness, "valence" : valence, "tempo": tempo}
+        features = sp.audio_features(uri)[0]
+        if features is None:
+            continue
+        dict[uri] = list(features.values())[:11] #only need necessary parameters
+    return dict
 #------------------------------------------------------------------------------#
 
 #RETRIEVING NECESSARY PLAYLISTS
@@ -94,6 +95,7 @@ top_songs_uris = []
 for i in range(num_top_tracks):
     top_songs_uris.append(top_songs_tracks[i]["track"]["uri"])
 
+
 num_missed_hits = len(missed_hits_tracks)
 missed_hits_uris = []
 for i in range(num_missed_hits):
@@ -107,28 +109,12 @@ for i in range(num_daily_tracks):
 test_uris = missed_hits_uris + daily_mix_uris
 
 test_songs_dict = getAudioFeatures(test_uris)
-top_songs_dict = getAudioFeatures(top_songs_uri[:80])
+top_songs_dict = getAudioFeatures(top_songs_uris[:79])
 
+test_iter = iter(test_songs_dict)
+top_iter = iter(top_songs_dict)
 
-
-# avg_dance = sum(danceability) / num_top_tracks
-# avg_energy = sum(energy) / num_top_tracks
-# avg_key = sum(key) / num_top_tracks
-# avg_loudness = sum(loudness) / num_top_tracks
-# avg_mode = sum(mode) / num_top_tracks
-# avg_speech = sum(speechiness) / num_top_tracks
-# avg_acoust = sum(acousticness) / num_top_tracks
-# avg_instr = sum(instrumentalness) / num_top_tracks
-# avg_live = sum(liveness) / num_top_tracks
-# avg_val = sum(valence) / num_top_tracks
-# avg_tempo = sum(tempo) / num_top_tracks
-#
-# print("average danceability: " + str(avg_dance))
-# print("average energy: " + str(avg_energy))
-# print("average loudness: " + str(avg_loudness))
-# print("average sppechiness: " + str(avg_speech))
-# print("average acousticness: " + str(avg_acoust))
-# print("average instrumentalness: " + str(avg_instr))
-# print("average liveness: " + str(avg_live))
-# print("average valence: " + str(avg_val))
-# print("average tempo: " + str(avg_tempo))
+for i in range(NUM_SONGS - 69):
+    test_uri = next(test_iter)
+    print("average similarity of " + sp.track(test_uri)["name"])
+    print(getAvgSimilarity(test_uri, test_songs_dict[test_uri], top_songs_dict))
